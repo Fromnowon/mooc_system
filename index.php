c149231421aa02fedc475bed2b2f4e89c3bf9e01
Fromnowon <zihangzhao@gmail.com>
2018-01-04 15:41:53 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        div{
+            margin: 10px;
+            background-color: red;
+            width: 100px;
+            height: 100px;
+        }
+    </style>
+</head>
+<body>
+<div class="div1"></div>
+<div class="div2"></div>
+<script>
+    $(".div1").click=function () {
+        alert("sasas");
+    }
+</script>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2018/1/4
+ * Time: 15:33
+ */
